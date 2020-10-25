53f23fe27e7f40fda4902b09544b702b13e49654
KYOUNGSOO-LEE <47710258+KYOUNGSOO-LEE@users.noreply.github.com>
2020-10-25 23:42:41 +0900

    20102523

--- app/src/main/java/com/ksteach81/caculator/MainActivity.java
+++ app/src/main/java/com/ksteach81/caculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.ksteach81.caculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +12,35 @@ public class MainActivity extends AppCompatActivity {
     int resultNumber = 0; // 계산된 결과 값을 저장하는 변수
     char operator = '+'; // 입력된 연산자를 저장하는 변수
 
-   TextView resultText;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         resultText=findViewById(R.id.result_text);
+    }
+
+    public void buttonClick(View view) {
+        if(view.getId() == R.id.all_clear_button){
+            isFirstInput = true;
+            resultNumber = 0;
+            operator = '+';
+            resultText.setTextColor(0xFF666666);
+            resultText.setText(String.valueOf(resultNumber));
+        }
+
+        if(view.getId() == R.id.num_1_button){
+            if(isFirstInput){
+                resultText.setTextColor(0xFF000000);
+                resultText.setText("1");
+                isFirstInput = false;
+            }else {
+                resultText.append("1");
+            }
+
+        }else {
+
+        }
     }
 }
